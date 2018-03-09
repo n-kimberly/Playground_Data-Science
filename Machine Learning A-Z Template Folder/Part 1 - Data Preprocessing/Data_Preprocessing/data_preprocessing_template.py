98c0f43d7151ad6c9ed453e973ec6278389483cf
n-kimberly <kimberlyvnguyen@gmail.com>
2018-03-08 21:10:19 -0800

    data pre-processing pt IV * Scale features via standardisation

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -55,14 +55,19 @@ Y = labelencoder_Y_fit
 ## dependent variables do not need to be dummy encoded
 
 # Splitting the dataset into the Training set and Test set
+# Import train_test_split
 from sklearn.cross_validation import train_test_split
-# split 20% for testing  using random number generator & assign train, test X, Y arrays
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+## split 20% for testing  using random number generator & assign train, test X, Y arrays
+X_train_unscaled, X_test_unscaled, Y_train_unscaled, Y_test_unscaled = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+# Feature Scaling so that age and salary are weighed the same
+## Import StandardScaler ((x_stand = (x - mean(x)) / (standard deviation(x))))
+from sklearn.preprocessing import StandardScaler
+## instantiate StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+## Transform train and test X arrays
+X_train = sc_X.fit_transform(X_train_unscaled)
+X_test = sc_X.transform(X_test_unscaled)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+## Transform train train Y array (test Y array will be predicted)
+Y_train = sc_y.fit_transform(Y_train_unscaled)
