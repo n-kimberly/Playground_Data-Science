d702a1d515a442b2da5d71638c353d07590131ad
nykimberly <kimberlyvnguyen@gmail.com>
2018-08-29 16:38:56 -0700

    remove old folders

--- PythonCC_DataVisualization/2_RandomWalk/main.py
+++
@@ -1,6 +0,0 @@
-from random_walk import RandomWalk
-import matplotlib.pyplot as plt
-
-a_walker = RandomWalk()
-a_walker.take_a_walk()
-a_walker.map_walk()
--- PythonCC_DataVisualization/2_RandomWalk/realtime_walking.py
+++
@@ -1,7 +0,0 @@
-from random_walk import RandomWalk
-import time
-import matplotlib.pyplot as plt
-
-a_walker = RandomWalk(5000, 2)
-a_walker.take_a_walk()
-# a_walker.map_walk()
